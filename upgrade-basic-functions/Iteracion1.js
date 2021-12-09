ac6fca2c8091f254aa7b4f0fed7df9af0c78b43b
unknown <yoel.arenas26@gmail.com>
2021-12-09 23:29:27 +0100

    Funciones Iteracion1

---
+++ upgrade-basic-functions/Iteracion1.js
@@ -0,0 +1,12 @@
+//Completa la función que tomando dos números como argumento devuelva el más alto.
+
+
+function sum(numberOne , numberTwo) {
+    if (numberOne > numberTwo){
+        return console.log(numberOne);
+    } else if (numberOne < numberTwo) {
+        return console.log(numberTwo);
+    }
+}
+
+sum ( 35, 40);
