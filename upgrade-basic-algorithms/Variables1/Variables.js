a6213696c253afae03732cd7e401cffc2a3ad39a
unknown <yoel.arenas26@gmail.com>
2021-12-09 19:38:26 +0100

    Variables1

---
+++ upgrade-basic-algorithms/Variables1/Variables.js
@@ -0,0 +1,16 @@
+//1.1
+let myFavoriteHero = 'Hulk';
+console.log(myFavoriteHero);
+
+//1.2
+let x = 50;
+console.log(x);
+
+//1.3
+let h = 5;
+let y = 10;
+console.log(h,y);
+
+//1.4
+let z = h + z;
+console.log(z);
