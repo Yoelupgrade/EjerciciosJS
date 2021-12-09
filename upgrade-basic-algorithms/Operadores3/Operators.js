17430d57b9a197661e6e0f3456799e9021857868
unknown <yoel.arenas26@gmail.com>
2021-12-09 20:42:50 +0100

    Operadores3

---
+++ upgrade-basic-algorithms/Operadores3/Operators.js
@@ -0,0 +1,19 @@
+//1.1
+console.log("10 * 5 = " + 10 * 5);
+
+//1.2
+console.log("10 / 2 = " + 10 / 2);
+
+//1.3
+console.log("el resto de dividir 15 / 9 es: " + (15 % 9));
+
+//1.4
+let y = 10;
+let z = 5;
+let x = y + z;
+console.log( y + ' + ' + z + ' = ' + x);
+
+//1.5
+let a = y * z;
+console.log( y + ' * ' + z + ' = ' + a);
+
